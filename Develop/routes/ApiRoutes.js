924c18ad6129bfc46ad2133aa5fc4d034a9ec732
Nate Wolfe <wolfe.nate26@gmail.com>
2022-06-30 13:33:18 -0600

    added in routes

---
+++ Develop/routes/ApiRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const fs = require("fs");
+let dbjson = require("../db/db.json");
+
+router.get("/notes", (req, res) => {
+  dbjson = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  console.log(dbjson);
+  res.json(dbjson);
+});
+
+router.post("/notes", (req, res) => {
+  let notetemplate = {
+    title: req.body.title,
+    text: req.body.text,
+    id: Math.floor(Math.random() * 100, 000),
+  };
+  dbjson.push(notetemplate);
+  fs.writeFileSync("./db/db.json", JSON.stringify(dbjson));
+  // res.json sends req changes to front end to render on screen
+  res.json(dbjson);
+});
+module.exports = router;
